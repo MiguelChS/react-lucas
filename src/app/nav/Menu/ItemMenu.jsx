0bc9e8e0b350c8803d6449e230d86423b56b0a3b
Chauca <mc185249@ncr.com>
2017-01-21 17:38:49 -0300

    agregando los evento de modal login

--- src/app/nav/Menu/ItemMenu.jsx
+++ src/app/nav/Menu/ItemMenu.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import Modal from '../../ComponentesCompartidos/Modal.jsx'
 
 export default class ButtonNavegador extends React.Component {
     constructor() {
         super();
+        this.state = {
+            modal: null
+        }
     }
+
+    click(){
+        this.setState({
+            modal : <Modal mostrar={true} prueba={this.onclose.bind(this)}/>
+        })
+
+    }
+    onclose(){
+        this.setState({
+            modal: null
+        })
+    }
+
     render(){
         return (
             <li>
-                <a role="button">Usuario</a>
+                <a role="button" onClick={this.click.bind(this)} >Usuario</a>
+                {this.state.modal}
             </li>
         )
     }
